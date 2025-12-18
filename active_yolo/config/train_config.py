097ae644e37380e0c84d3542eeffe39392695287
SumedhP <43325361+SumedhP@users.noreply.github.com>
2025-12-18 11:44:43 -0800

    Correct unneeded load commands in train config

--- active_yolo/config/train_config.py
+++ active_yolo/config/train_config.py
@@ -14,12 +14,6 @@ class AugmentationConfig:
     mosaic: float
     scale: float
 
-    @staticmethod
-    def load_from_yaml(file_path: str) -> "AugmentationConfig":
-        with open(file_path, "r") as f:
-            return AugmentationConfig(**yaml.safe_load(f))
-
-
 @dataclass
 class YOLOTrainConfig:
     model: str  # Model path
@@ -35,13 +29,6 @@ class YOLOTrainConfig:
     optimizer: str
 
     augmentation: AugmentationConfig
-
-    @staticmethod
-    def load_from_yaml(file_path: str) -> "YOLOTrainConfig":
-        with open(file_path, "r") as f:
-            cfg = yaml.safe_load(f)
-        cfg["augmentation"] = AugmentationConfig(**cfg["augmentation"])
-        return YOLOTrainConfig(**cfg)
 
     @staticmethod
     def load_from_dict(cfg: dict) -> "YOLOTrainConfig":
@@ -63,11 +50,6 @@ class SSLTrainConfig:
     overwrite: bool
 
     teacher: str
-
-    @staticmethod
-    def load_from_yaml(file_path: str) -> "SSLTrainConfig":
-        with open(file_path, "r") as f:
-            return SSLTrainConfig(**yaml.safe_load(f))
 
     @staticmethod
     def load_from_dict(cfg: dict) -> "SSLTrainConfig":
