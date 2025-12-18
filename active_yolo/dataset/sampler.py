edd5beba0c874c26632f57b309e9b5f46e494637
SumedhP <43325361+SumedhP@users.noreply.github.com>
2025-12-18 11:30:51 -0800

    Update sampler to fill images till validation meets amount needed

--- active_yolo/dataset/sampler.py
+++ active_yolo/dataset/sampler.py
@@ -1,28 +1,27 @@
 from collections import Counter
 from typing import List, Tuple
-from label import Label
+from ..label.label import Label
+
 
 def stratified_split(
     labels: List[Label], val_ratio: float
 ) -> Tuple[List[Label], List[Label]]:
     total_class_counts = Counter()
     image_class_counts = []
-    
+
     for label in labels:
         class_counts = label.get_class_ids()
         total_class_counts.update(class_counts)
         image_class_counts.append(class_counts)
-        
+
     desired_class_counts = {
-        cls: int(count * val_ratio)
-        for cls, count in total_class_counts.items()
+        cls: int(count * val_ratio) for cls, count in total_class_counts.items()
     }
-    
+
     current_val_counts = Counter()
     val_set = []
     train_set = []
-    
-    
+
     def _rarity_score(idx):
         # Calculate the rarity score based on the class counts
         # Something feels fishy about this logic
@@ -31,23 +30,33 @@ def stratified_split(
             score += 1.0 / total_class_counts[cls]
         return score
 
-    idxs = list(range(len(labels)))
-    idxs.sort(key=_rarity_score, reverse=True)
-    
+    indices = list(range(len(labels)))
+    indices.sort(key=_rarity_score, reverse=True)
+
+    val_set_indices = []
+
     # Time for greed
-    for idx in idxs:
+    for idx in indices:
         class_counts = image_class_counts[idx]
-        
+
         fills_need = False
         for cls in class_counts:
             if current_val_counts[cls] < desired_class_counts[cls]:
                 fills_need = True
                 break
-        
+
         if fills_need:
-            val_set.append(labels[idx])
+            val_set_indices.append(idx)
             current_val_counts.update(class_counts)
-        else:
-            train_set.append(labels[idx])
-    
+
+    # If we don't have enough labels in the validation set, add more:
+    while len(val_set_indices) < int(len(labels) * val_ratio):
+        for idx in indices:
+            if idx not in val_set_indices:
+                val_set_indices.append(idx)
+                break
+
+    val_set = [labels[idx] for idx in val_set_indices]
+    train_set = [label for label in labels if label not in val_set]
+
     return train_set, val_set
